3076d8f05ff7c3e22e40bba1ad28336df8f31863
GitHub Action <action@github.com>
2025-04-07 13:28:43 +0000

    Initialize template repository proper
    Set package name to com.github.riccardomascotto.kuudrasorttest
    Set project name to KuudraSortTest
    Set modid to kuudrasorttest

--- src/main/java/com/github/riccardomascotto/kuudrasorttest/ExampleMod.java
+++ src/main/java/com/github/riccardomascotto/kuudrasorttest/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.riccardomascotto.kuudrasorttest;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "kuudrasorttest", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
